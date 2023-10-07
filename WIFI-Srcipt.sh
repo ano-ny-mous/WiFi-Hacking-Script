759e795fad9e08138b825de2cc0fe35fb3a09c36
ano-ny-mous <nesar6174@gmail.com>
2023-10-07 22:36:58 +0530

    Add files via upload

---
+++ WIFI-Srcipt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check for superuser privileges
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root. Please use sudo."
+  exit 1
+fi
+
+# Disable [interface_name] interface
+echo "Disabling [interface_name] interface..."
+ifconfig [interface_name] down
+
+# Change the MAC address of [interface_name]
+echo "Changing the MAC address of ..."
+macchanger -r [interface_name]
+
+# Enable [interface_name] interface
+echo "Enabling [interface_name] interface..."
+ifconfig  up
+
+# Run wifite
+echo "Running wifite..."
+wifite
+
+# Script complete
+echo "Script execution complete."
+
